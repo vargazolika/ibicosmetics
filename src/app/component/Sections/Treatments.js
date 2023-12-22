770c8865b7d32a121df6c6b3ac30f6b6c8303cde
Edvin Talmácsi <etalmacsi@monguz.hu>
2023-12-22 19:20:58 +0100

    treatments

--- src/app/component/Sections/Treatments.js
+++ src/app/component/Sections/Treatments.js
@@ -1,10 +1,25 @@
+import Image from "next/image";
+
+const TREATMENTS = [
+  { img: "/facial.jpg", title: "Arckezelésink" },
+  { img: "/body.jpg", title: "Testkezeléseink" },
+  { img: "/hands.jpg", title: "Kéz és láb" },
+  { img: "/hair.jpg", title: "Szőrtelenítés" },
+  { img: "/makeup.jpg", title: "Smink" },
+  { img: "/additional.jpg", title: "További kezeléseink" },
+];
+
 export default function Treatments(props) {
-    return (
-      <div className="treatments">
-        <div>
-          <h1>Kezelések</h1>
-        </div>
+  return (
+    <div className="treatments">
+      <div className="treatments-wrapper">
+        {TREATMENTS.map((item, index) => (
+          <div className="treatment" key={index}>
+            <Image src={item.img} width={250} height={509} alt={item.title} />
+            <h4 className="treatment-title">{item.title}</h4>
+          </div>
+        ))}
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -11,10 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-      />
       <body className={inter.className}>{children}</body>
     </html>
   );
